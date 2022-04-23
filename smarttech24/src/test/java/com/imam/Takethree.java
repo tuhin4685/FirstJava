c879d4f6689ae51ca4d5c4a44f94a2910b21048f
mhasa <mhasa@DESKTOP-9AOTDO3.lan>
2022-04-23 17:50:22 -0400

    take three

---
+++ smarttech24/src/test/java/com/imam/Takethree.java
@@ -0,0 +1,7 @@
+package com.imam;
+
+public class Takethree {
+public static void main(String[] args) {
+	System.out.println("Hi Java");
+}
+}
---
+++ smarttech24/src/test/java/com/imam/Trial.java
@@ -0,0 +1,7 @@
+package com.imam;
+
+public class Trial {
+public static void main(String[] args) {
+	System.out.println("hi selenium");
+}
+}
